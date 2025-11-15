46a9cdd705c985e9cc22bc68c61c79443e8a71b4
syauqi <m.syauqi357@gmail.com>
2025-11-15 17:32:25 +0700

    add layout by positioning the content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -75,7 +75,7 @@ Sidebar.svelte = Imported component that displays navigation items
 	<Sidebar bind:sidebarOpen {navItems} />
 
 	<div class="p-4 sm:ml-64">
-		<div class="mt-14 p-4">
+		<div class="mt-21 p-4">
 
 			<!-- position of pages -->
 			<slot />
