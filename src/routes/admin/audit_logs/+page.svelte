d145854bfa5cfb7166cd216eea3f23338999246d
syauqi <m.syauqi357@gmail.com>
2025-11-20 01:07:49 +0700

    layout head

--- src/routes/admin/audit_logs/+page.svelte
+++ src/routes/admin/audit_logs/+page.svelte
@@ -3,8 +3,17 @@
 
 <div class="w-full">
 	<!-- header -->
-	<div class="w-full bg-slate-400 p-4">
-		<h1 class="sm:text-3xl text-xl capitalize font-semibold tracking-wide">audit log</h1>
-        <!-- feature sorter -->
+	<div class="sm:flex flex-col sm:flex-row w-full items-center gap-3 hover:bg-slate-400 ease-in-out transition-all rounded-2xl p-4">
+		<h1 class="text-xl font-semibold tracking-wide capitalize sm:text-3xl">audit log</h1>
+		<!-- feature sorter -->
+		<div class=" p-3 flex items-center gap-3">
+			<!-- put logic right here -->
+			<button
+				class="rounded-md bg-purple-500 p-2 pr-3 pl-3 text-xs text-purple-100 outline-2 outline-offset-0 outline-purple-700 transition-all duration-150 ease-in-out outline-solid hover:bg-purple-800 hover:outline-offset-2 sm:text-sm"
+			>
+				this will be a button
+			</button>
+            <p>this is a paragraph</p>
+		</div>
 	</div>
 </div>
