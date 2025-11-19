e1a1184ea14e47b93105fa36693539bf48123f64
syauqi <m.syauqi357@gmail.com>
2025-11-20 00:09:53 +0700

    start new feature of audit logs with layout

---
+++ src/routes/admin/audit_logs/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div class="w-full">
+	<!-- header -->
+	<div class="w-full bg-slate-400 p-4">
+		<h1 class="sm:text-3xl text-xl capitalize font-semibold tracking-wide">audit log</h1>
+        <!-- feature sorter -->
+	</div>
+</div>
