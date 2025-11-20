a77d255ca8c5bcc9a5e97cd3ebe4f875196f71b4
SYAUQI <m.syauqi357@gmail.com>
2025-11-20 20:15:29 +0700

    changing school name

--- backend/routes/api/schooldataNav.js
+++ backend/routes/api/schooldataNav.js
@@ -1,9 +1,10 @@
 // School data
 const schoolData = {
-	name: 'MTs. Persis 2 Bangil',
-	npsn: '23169874273289',
+	name: 'MTs. Al-Hasyimiy',
+	npsn: '63747632892636',
     // 231698134 <- number before
-    // 2316989832 <- number after 
+    // 2316989832 <- number after
+     // 23169874273289 <- number after changes and replacement
     // prerequisites : harus di reload dulu server nya biar ngambil data, ini nanti di ganti sama query backend
 	logoUrl: 'upload/' // Frontend will use default logo if empty
 };
