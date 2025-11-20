55a10477781d1156a6544d50c5dac2f211045f62
SYAUQI <m.syauqi357@gmail.com>
2025-11-20 19:16:43 +0700

    adding static data json

---
+++ backend/routes/api/schooldataNav.js
@@ -0,0 +1,11 @@
+// School data
+const schoolData = {
+	name: 'MTs. Persis 2 Bangil',
+	npsn: '23169874273289',
+    // 231698134 <- number before
+    // 2316989832 <- number after 
+    // prerequisites : harus di reload dulu server nya biar ngambil data, ini nanti di ganti sama query backend
+	logoUrl: 'upload/' // Frontend will use default logo if empty
+};
+
+export default schoolData;
