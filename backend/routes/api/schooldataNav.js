7751201b0c302ad4fcb575759b96703c90a041d7
syauqi <m.syauqi357@gmail.com>
2025-11-21 12:17:58 +0700

    add another data for prepare

--- backend/routes/api/schooldataNav.js
+++ backend/routes/api/schooldataNav.js
@@ -2,6 +2,10 @@
 const schoolData = {
 	name: 'MTs. Al-Hasyimiy',
 	npsn: '63747632892636',
+    nsm: '73210987219',
+    akreditasi: 'B',
+    alamat:'jalan kh abdul rahman wahid no.3 gang. 7',
+    negara:'indonesia',
     // 231698134 <- number before
     // 2316989832 <- number after
      // 23169874273289 <- number after changes and replacement
