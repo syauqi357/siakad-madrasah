f3468e34c7263bbf553384ab885f2b8ce89f35aa
syauqi <m.syauqi357@gmail.com>
2025-12-06 14:48:45 +0700

    add some feature on sidebar, add static data on auditlogs,change head title

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -10,7 +10,7 @@ Summary of Changes:
 	   When you get data from your API, it will use the logoUrl from the API if it exists. If the API doesn't return a logoUrl, 
         it will fall back to using the logo you imported. This makes your component more robust.
 
-// generate by gemini
+		// generate by gemini
 	
 	*/
 	import { onMount } from 'svelte';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import Navbar from '$lib/components/layout/Navbar.svelte';
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
+	
+
+	
 
 	let sidebarOpen = false;
 	let openDropdowns: Record<string, boolean> = {};
@@ -9,22 +12,42 @@
 	const navItems = [
 		{
 			name: 'Dashboard',
-			icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" id="Google-Home-Logo--Streamline-Logos" height="16" width="16">  <path fill="#000000" fill-rule="evenodd" d="M8.942666666666666 1.3906666666666665a1.3333333333333333 1.3333333333333333 0 0 0 -1.885333333333333 0l-5.666666666666666 5.666666666666666A1.3333333333333333 1.3333333333333333 0 0 0 1 8v3.2133333333333334L9.882666666666665 2.3306666666666667l-0.94 -0.94Zm1.6473333333333333 1.6466666666666667 -1.885333333333333 1.8860000000000001 3.6286666666666663 3.6286666666666663V12.666666666666666l0 2.6666666666666665h1.3333333333333333a1.3333333333333333 1.3333333333333333 0 0 0 1.3333333333333333 -1.3333333333333333v-6a1.3333333333333333 1.3333333333333333 0 0 0 -0.3906666666666666 -0.9426666666666665l-4.019333333333333 -4.02ZM11.333333333333332 15.333333333333332v-2.6666666666666665H4.666666666666666v2.6666666666666665h6.666666666666666ZM3.6666666666666665 15.333333333333332v-3l0 -2.3726666666666665 -2.6666666666666665 2.6666666666666665V14a1.3333333333333333 1.3333333333333333 0 0 0 1.3333333333333333 1.3333333333333333h1.3333333333333333Z" clip-rule="evenodd" stroke-width="0.6667"></path></svg>', 
+			icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" id="Google-Home-Logo--Streamline-Logos" height="16" width="16">  <path fill="#000000" fill-rule="evenodd" d="M8.942666666666666 1.3906666666666665a1.3333333333333333 1.3333333333333333 0 0 0 -1.885333333333333 0l-5.666666666666666 5.666666666666666A1.3333333333333333 1.3333333333333333 0 0 0 1 8v3.2133333333333334L9.882666666666665 2.3306666666666667l-0.94 -0.94Zm1.6473333333333333 1.6466666666666667 -1.885333333333333 1.8860000000000001 3.6286666666666663 3.6286666666666663V12.666666666666666l0 2.6666666666666665h1.3333333333333333a1.3333333333333333 1.3333333333333333 0 0 0 1.3333333333333333 -1.3333333333333333v-6a1.3333333333333333 1.3333333333333333 0 0 0 -0.3906666666666666 -0.9426666666666665l-4.019333333333333 -4.02ZM11.333333333333332 15.333333333333332v-2.6666666666666665H4.666666666666666v2.6666666666666665h6.666666666666666ZM3.6666666666666665 15.333333333333332v-3l0 -2.3726666666666665 -2.6666666666666665 2.6666666666666665V14a1.3333333333333333 1.3333333333333333 0 0 0 1.3333333333333333 1.3333333333333333h1.3333333333333333Z" clip-rule="evenodd" stroke-width="0.6667"></path></svg>',
 			href: '/'
 		},
+
 		{
-			name: 'Rombel',
-			href: '/rombel'
+			name: 'sarana prasarana',
+			href: '/sarpras',
+			hasDropdown: true,
+			children: [
+				{
+					name: 'aset tetap',
+					icon: '',
+					href: 'sarpras/asettetap'
+				},
+				{
+					name: 'aset lancar',
+					icon: '',
+					href: 'sarpras/asetlancar'
+				},
+				{
+					name: 'perpustakaan',
+					icon: '',
+					href: 'sarpras/perpustakaan'
+				}
+			]
 		},
+
 		{
 			name: 'Student',
 			href: '/siswa',
 			hasDropdown: true,
 			children: [
-				{ 
+				{
 					name: 'Student List',
-					icon: '', 
-					href: '/siswa', 
+					icon: '',
+					href: '/siswa'
 				},
 				{ name: 'Alumni', href: '/siswa/alumni' },
 				{ name: 'Mutasi Out', href: '/siswa/mutasi-out' },
@@ -34,6 +57,104 @@
 				{ name: 'PIP/KIP-K/Beasiswa', href: '/siswa/beasiswa' }
 			]
 		},
+
+		{
+			name: 'Rombel',
+			href: '/rombel'
+		},
+
+		{
+			name: 'guru & tendik',
+			href: '/gurutendik',
+			hasDropdown: true,
+			children: [
+				{
+					name: 'daftar GTK',
+					href: ''
+				},
+				{
+					name: 'pengajuan GTK',
+					href: ''
+				},
+				{
+					name: 'mutasi',
+					href: ''
+				},
+				{
+					name: 'akun',
+					href: ''
+				}
+			]
+		},
+
+		{
+			name: 'nilai siswa',
+			href: '/score',
+			hasDropdown: true,
+			children: [
+				{
+					name: 'ujian',
+					href: ''
+				},
+				{
+					name: 'tugas',
+					href: ''
+				},
+				{
+					name: 'mata pelajaran',
+					href: ''
+				},
+				{
+					name: 'kenaikan kelas',
+					href: ''
+				}
+			]
+		},
+
+		{
+			name: 'konfirmasi',
+			href: '/confirm',
+			hasDropdown: true,
+			children: [
+				{
+					name: 'kelembagaan',
+					href: 'confirm/lembaga'
+				},
+				{
+					name: 'sarana prasarana',
+					href: ''
+				},
+				{
+					name: 'siswa',
+					href: ''
+				},
+				{
+					name: 'guru tendik',
+					href: ''
+				}
+				// {
+				// 	name:'',
+				// 	href:''
+				// },
+			]
+		},
+
+		{
+			name: 'info lainnya',
+			href: '',
+			hasDropdown: true,
+			children: [
+				{
+					name: 'daftar kurikulum',
+					href: ''
+				},
+				{
+					name: 'hari sekolah',
+					href: ''
+				}
+			]
+		},
+
 		{
 			name: 'Audit Logs',
 			href: '/admin/audit_logs'
@@ -46,7 +167,7 @@
 </script>
 
 <svelte:head>
-	<title>MyApp</title>
+	<title>sistem Akademik </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
 
