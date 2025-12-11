02a18164b50bc75d4e46b0ad394a3731e3543d48
SYAUQI <m.syauqi357@gmail.com>
2025-12-11 19:25:58 +0700

    add env and change to env port

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
-<script></script>
+<script lang="ts">
+	// logic for turn out
 
-<div class="bg-slate-100 min-h-screen">
-   <a href="/login">
-   <button>
-      login
-   </button>
-   </a>
+</script>
+
+<div class="min-h-screen">
+	<div class="flex h-screen items-center justify-center flex-col gap-24">
+		<a href="/login">
+			<button class="hover:bg-blue-700 gap-2 bg-blue-500 ease-in-out transition-all duration-200 flex items-center justify-center p-3 pr-5 pl-5 text-white rounded-md">
+				<span class="bg-amber-300 w-6 h-6">
+
+				</span>
+				login
+			</button>
+		</a>
+		<div>
+			<table>
+				<thead>
+					<tr>
+						<td>1</td>
+						<td>2</td>
+						<td>3</td>
+						<td>4</td>
+					</tr>
+				</thead>
+			</table>
+		</div>
+	</div>
 </div>
