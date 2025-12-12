46a6ae53ce5be414a369441784d8bf68aa1d140b
syauqi <m.syauqi357@gmail.com>
2025-12-12 17:26:14 +0700

    delete register and change style dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 
 	// source modify : https://dev.to/digitaldrreamer/change-your-sveltekit-or-any-vite-powered-dev-server-localhost-port-2e2h
 	server: {
-		port: 1243
+		port: 1245
 	},
 	preview: {
 		port: 1243
