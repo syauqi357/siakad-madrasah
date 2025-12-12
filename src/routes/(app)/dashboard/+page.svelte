835d51ac97e652d43185985580090c8670523b9f
syauqi <m.syauqi357@gmail.com>
2025-12-12 17:26:14 +0700

    delete register and change style dashboard

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -16,9 +16,9 @@
 	<h1 class="text-3xl font-bold mb-6">Dashboard</h1>
 
 	{#if user}
-		<div class="bg-white p-6 rounded-lg shadow">
-			<p class="text-lg">Welcome back, <strong>{user.username}</strong>!</p>
-			<p class="text-gray-600">Your role: <strong class="text-purple-600">{user.role}</strong></p>
+		<div class="bg-slate-100 p-6 rounded-lg border border-slate-300">
+			<p class="text-lg capitalize">selamat datang, <strong class="capitalize text-blue-700">{user.username}</strong>!</p>
+			<p class="text-gray-600">sebagai: <strong class="capitalize text-slate-600">{user.role}</strong></p>
 		</div>
 	{/if}
 </div>
