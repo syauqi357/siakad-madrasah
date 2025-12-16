69e7736bc979f2d76e6a77a97cddde928c8b9cf2
SYAUQI <m.syauqi357@gmail.com>
2025-12-16 15:15:45 +0700

    add new feature

--- src/routes/(app)/admin/profile/+page.svelte
+++ src/routes/(app)/admin/profile/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 import { onMount } from 'svelte';
 import ChangePassword from '$lib/components/layout/changePassword.svelte';
-
+import ChangeUsername from '$lib/components/layout/changeUsername.svelte';
 </script>
-<div class="flex min-h-screen items-center justify-center">
+<div class="p-5 flex">
      <ChangePassword />
+     <!-- <changeUsername /> -->
+     <ChangeUsername/>
 </div>
