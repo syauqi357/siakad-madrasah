f2e04da02cb4832f03aa84ab83a5223f014ae012
syauqi <m.syauqi357@gmail.com>
2026-01-12 16:57:40 +0700

    prettier

--- backend/src/db/schema/studentsdataTable.js
+++ backend/src/db/schema/studentsdataTable.js
@@ -1,22 +1,23 @@
 import { sqliteTable, int, text } from 'drizzle-orm/sqlite-core';
 
 export const studentTable = sqliteTable('student', {
-  id: int().primaryKey({ autoIncrement: true }),
-  nisn: int().notNull(),
-  nis: int().notNull(),
-  name: text().notNull(),
-  class: text().notNull(),
-  gender: text('gender', { enum: ['Laki-laki', 'Perempuan'] }).notNull(),
-  cityOfOrigin: text().notNull(),
-  status: text('status', {
-    enum: ['active', 'nonactive', 'warning', 'alumni']
-  }).notNull().default('active'),
-  age: int().notNull(),
-  address: text().notNull()
+	id: int().primaryKey({ autoIncrement: true }),
+	nisn: int().notNull(),
+	nis: int().notNull(),
+	name: text().notNull(),
+	class: text().notNull(),
+	gender: text('gender', { enum: ['Laki-laki', 'Perempuan'] }).notNull(),
+	cityOfOrigin: text().notNull(),
+	status: text('status', {
+		enum: ['active', 'nonactive', 'warning', 'alumni']
+	})
+		.notNull()
+		.default('active'),
+	age: int().notNull(),
+	address: text().notNull()
 
-  // indexing in id and nisn and name.
-  //id nisn and name is used frequently and often be called, for suggestion on the db need to use this column to indexed as well
-  
+	// indexing in id and nisn and name.
+	//id nisn and name is used frequently and often be called, for suggestion on the db need to use this column to indexed as well
 });
 
 /*
