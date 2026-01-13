440afed1ae4ef97253093a1da1913cbc7dd9d904
syauqi <m.syauqi357@gmail.com>
2026-01-13 09:29:34 +0700

    add to index

--- backend/src/db/schema/studentAddress.js
+++ backend/src/db/schema/studentAddress.js
@@ -1,5 +1,4 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-import { sql } from 'drizzle-orm';
 import { studentTable } from './studentsdataTable.js';
 
 export const studentAddress = sqliteTable('studentAddress', {
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,9 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import { studentTable } from './db/schema/studentsdataTable.js';
 import { schoolDataTable } from './db/schema/schooldataTable.js';
+import { auditTable } from './db/schema/auditlog.js';
 
 const sqlite = new Database(process.env.DATABASE_URL);
 export const db = drizzle(sqlite);
 
-export { studentTable, schoolDataTable };
+export { studentTable, schoolDataTable, auditTable };
