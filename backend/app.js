8491e8bbebed988ff4cdbc6801550f5973df1441
syauqi <m.syauqi357@gmail.com>
2026-01-13 20:42:04 +0700

    add : rate limit on the middleware

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ import auditLogsRouter from './routes/auditLog/APILogs/audit_logs.js';
 import { auditLog } from './middlewares/middlewareAudit.js'; // Import audit middlewares
 import dotenv from 'dotenv';
 import path from 'path'; // Import path module
+import { GlobalRatelimit } from './middlewares/globalRatelimit/rateLimiter.js';
+import { speedLimit } from './middlewares/throttleFeat/throttleLimit.js';
 
 // This line loads the environment variables from a .env file into process.env
 dotenv.config();
@@ -15,6 +17,7 @@ const FE_port = process.env.FRONTEND_URL_DEV;
 // const FE_port_prod = process.env.FRONTEND_URL_;
 const PORT = process.env.PORT;
 
+const address = process.env.ADDRESS_SERVER;
 const corsOptions = {
 	origin: FE_port,
 	credentials: true,
@@ -23,22 +26,25 @@ const corsOptions = {
 // header line
 app.use(cors(corsOptions));
 // restAPI
-app.use(express.json());
 
+app.use(express.json());
+app.use(GlobalRatelimit);
 // Apply Audit Log Middleware globally
 // This ensures all API requests are logged
-app.use(auditLog);
 
+app.use(auditLog);
 // Serve static files from the '(public)' directory
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// middlewares statis
+// static middleware for upload data or images
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/upload', express.static(path.join(__dirname, 'upload')));
 
 // 2. Use a more standard API route
+app.use('/routes/api', speedLimit);
 // - changing route using data from API for school data navbar set
 app.use('/routes/api', schoolDataRouter);
 app.use('/routes/api', studentDataRouter);
@@ -51,6 +57,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-	console.log(`✅ Server running at http://localhost:${PORT}`);
+	console.log(`✅ Server running at ${address}:${PORT}`);
 	console.log('✅ database running at:', process.env.DATABASE_URL);
 });
---
+++ backend/middlewares/globalRatelimit/rateLimiter.js
@@ -0,0 +1,21 @@
+import rateLimit from 'express-rate-limit';
+
+export const GlobalRatelimit = rateLimit({
+	/*
+	 *
+	 * catch source from :
+	 * https://stackoverflow.com/questions/58486616/express-rate-limit-catching-the-message
+	 *
+	 * docs :
+	 * https://www.npmjs.com/package/express-rate-limit
+	 * */
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	message: 'too many request, please try again few minutes later',
+	max: 300,
+	standardHeaders: true,
+	legacyHeaders: false,
+	statusCode: 429, // 429 status = Too Many Requests (RFC 6585)
+	headers: true, //Send custom rate limit header with limit and remaining
+	skipFailedRequests: false, // Do not count failed requests (status >= 400)
+	skipSuccessfulRequests: false // Do not count successful requests (status < 400)
+});
