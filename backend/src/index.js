ef0249d4096fa11644ca5f3f98390537e070da6b
syauqi <m.syauqi357@gmail.com>
2026-01-16 23:32:59 +0700

    add relations to index

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,10 @@ import Database from 'better-sqlite3';
 import { studentTable } from './db/schema/studentsdataTable.js';
 import { schoolDataTable } from './db/schema/schooldataTable.js';
 import { auditTable } from './db/schema/auditlog.js';
+import { teachersRelations, subjectsRelations, academicYearsRelations, classesRelations, rombelRelations, classSubjectsRelations } from './db/schema/relations.js'
+
 
 const sqlite = new Database(process.env.DATABASE_URL);
 export const db = drizzle(sqlite);
 
-export { studentTable, schoolDataTable, auditTable };
+export { studentTable, schoolDataTable, auditTable, teachersRelations, subjectsRelations, academicYearsRelations, classesRelations, rombelRelations, classSubjectsRelations };
