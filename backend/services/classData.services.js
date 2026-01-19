1c877ef40ab0dcca004e7b311a9c63fc2afbf758
syauqi <m.syauqi357@gmail.com>
2026-01-19 15:57:18 +0700

    add class services to process all class data

--- backend/services/classData.services.js
+++ backend/services/classData.services.js
@@ -13,4 +13,50 @@
  * 	className: text('class_name').notNull() // X, XI, XII
  * });
  *
+ * class data is used too with academic year  and related to this schema drizzle :
+ * backend/src/db/schema/academicYear.js
+ * its looks like this :
+ *
+ * import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+ *
+ * export const academicYear = sqliteTable('academic_year', {
+ * 	id: integer('id').primaryKey({ autoIncrement: true }),
+ *
+ * 	name: text('name').notNull().unique(),
+ *
+ * 	startYear: integer('start_year'),
+ * 	endYear: integer('end_year'),
+ *
+ * 	startDate: text('start_date'), // SQLite DATE = TEXT
+ * 	endDate: text('end_date'),
+ *
+ * 	isActive: integer('is_active').default(0) // BOOLEAN → INTEGER (0/1)
+ * });
+ *
  * */
+
+// input academic year
+export const academicYearAcceptance = async () => {
+
+};
+
+// input class data
+export const inputClassData = async () => {
+
+};
+
+// edit class data
+export const editClassData = async () => {
+
+};
+
+// get class data to show it as a dropdown or card view
+export const getClassData = async () => {
+
+};
+
+// delete class data
+export const deleteClassData = async () => {
+
+};
+
